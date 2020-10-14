8025c798d12e48019b29f735519229b04b2c35e7
huangb <androidleo@outlook.com>
2020-10-14 17:00:53 +0800

    --

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Square 组件渲染了一个单独的 <button>
+class Square extends React.Component {
+  render() {
+    return (
+      // <button className="square" onClick={function() {alert('click'); }}>
+      <button className="square" onClick={() => alert('click')}>
+        {/* 数据通过 props 的传递，从父组件流向子组件 */}
+        {this.props.value}
+      </button>
+    );
+  }
+}
+// Board 组件渲染了 9 个方块。
+class Board extends React.Component {
+  renderSquare(i) {
+    // 传递一个名为 value 的 prop 到 Square 当中
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+// Game 组件渲染了含有默认值的一个棋盘
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
