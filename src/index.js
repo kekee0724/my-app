03cf706d2aeeb6b02974f3f03b5ead224f371ba8
huangb <androidleo@outlook.com>
2020-10-15 13:28:29 +0800

    --

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import './index.css';
 // Square 组件渲染了一个单独的 <button>
 class Square extends React.Component {
   // 向这个 class 中添加一个构造函数，用来初始化 state
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     value: null,
+  //   };
+  // }
   render() {
     return (
       // <button className="square" onClick={function() {alert('click'); }}>
       <button
         className="square"
-        onClick={() => this.setState({ value: 'X' })}>
+        onClick={() => this.props.onClick()}>
         {/* 数据通过 props 的传递，从父组件流向子组件 */}
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -32,7 +32,12 @@ class Board extends React.Component {
   }
   renderSquare(i) {
     // 传递一个名为 value 的 prop 到 Square 当中
-    return <Square value={this.state.squares[i]} />;
+    return (
+        <Square
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)}
+        />
+        );
   }
 
   render() {
@@ -59,6 +64,12 @@ class Board extends React.Component {
       </div>
     );
   }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] ='x';
+      this.setState({squares:squares});
+    }
 }
 // Game 组件渲染了含有默认值的一个棋盘
 class Game extends React.Component {
